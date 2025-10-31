4c9248cf4cb77a31b32517e3c14008702b12ab41
Niyas <122938174+mkniyas@users.noreply.github.com>
2025-10-31 08:47:36 -0500

    Added src/main/java/com/patientpoint/ens/AppMain.java

---
+++ src/main/java/com/patientpoint/ens/AppMain.java
@@ -0,0 +1,14 @@
+package com.patientpoint.ens;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+public class AppMain {
+  public static void main(String[] args) { SpringApplication.run(AppMain.class, args); }
+}
+@RestController
+class HelloController {
+  @GetMapping("/")
+  String hello() { return "ens is alive!"; }
+}
