3ee474717abde5fb7d867440f346b144c055fabc
Niyas <122938174+mkniyas@users.noreply.github.com>
2025-10-31 08:42:21 -0500

    Added src/test/java/com/patientpoint/ens/AppMainTests.java

---
+++ src/test/java/com/patientpoint/ens/AppMainTests.java
@@ -0,0 +1,7 @@
+package com.patientpoint.ens;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class AppMainTests {
+  @Test void contextLoads() {}
+}
